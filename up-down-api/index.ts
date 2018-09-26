224dfe3a8fbadb832cb00d46541317f51e5cb5a7
Raiondesu <a.raiondesu@gmail.com>
2018-09-26 20:33:13 +0300

    Simple server with routes & preset for future projects

---
+++ up-down-api/index.ts
@@ -0,0 +1,97 @@
+/**
+ * Entry API file
+ */
+import { createServer } from 'http';
+import { parse as parseUrl } from 'url';
+import { StringDecoder } from 'string_decoder';
+import { fromPath, route } from './misc';
+
+const server = createServer((req, res) => {
+  // Get url and parse it
+  const parsedUrl = parseUrl(req.url || '', true);
+
+  // Get path and query params
+  const trimmedPath = parsedUrl.pathname!.replace(/(^\/+)|(\/+$)/g, '');
+  const query = parsedUrl.query;
+
+  // Get request method
+  const method = req.method!;
+
+  // Get headers as object
+  const headers = req.headers;
+
+  // Parse payload if any
+  const decoder = new StringDecoder('UTF-8');
+
+  let buffer = '';
+
+  req.addListener('data', chunk => {
+    buffer += decoder.write(chunk);
+  });
+
+  req.addListener('end', async () => {
+    buffer += decoder.end();
+
+    // Choose the route handler
+    const handler = fromPath(router, trimmedPath, '/') || router['*'];
+
+    // Construct data to send to the handler
+    const data = {
+      trimmedPath,
+      query,
+      headers,
+      payload: buffer
+    };
+
+    // Route the request to the handler
+    const handlerData = await handler(data);
+
+    const statusCode = typeof handlerData.status === 'number' ? handlerData.status : 200;
+    const payload = typeof handlerData.payload === 'undefined' ? {} : handlerData.payload;
+
+    const payloadString = JSON.stringify(payload);
+
+    res.setHeader('Content-Type', 'application/json');
+    res.writeHead(statusCode);
+    res.end(payloadString);
+
+    console.log(`${statusCode}: Returning %s to %s %s`, payloadString, method, trimmedPath);
+  });
+});
+
+server.listen(3000, () => console.log('Server listening on port 3000.'));
+
+const router: IRouteTree = {
+  // Sample handler
+  'sample': route(_data => ({
+    status: 406,
+    payload: {
+      name: 'Sample handler'
+    }
+  }), {
+    'test': _data => ({
+      status: 200,
+      payload: {
+        test: 'Test 2-nd level handler'
+      }
+    })
+  }),
+
+  // 404 handler
+  '*': () => ({ status: 404 })
+};
+
+export type RoutePayload<T> = {
+  status?: number;
+  payload?: T;
+};
+
+export type RouteHandler<T = any> = ((data: any) => Promise<RoutePayload<T>> | {});
+
+export type RouteHandlerWithChildren<T extends undefined | {
+  [child: string]: RouteHandlerWithChildren;
+} = any> = T extends undefined ? RouteHandler : (RouteHandler & T);
+
+export interface IRouteTree {
+  [handler: string]: RouteHandlerWithChildren;
+}
